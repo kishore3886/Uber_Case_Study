198907dfe0cb808316b39a88e7ac880c9bee4dee
kishore <kishore3886@gmail.com>
2017-06-02 13:08:11 -0400

    created seperate data frame for completed status and calculated travel time for each entry

--- uber_Case_Study.R
+++ uber_Case_Study.R
@@ -2,6 +2,8 @@
 #installing neccessary packages to deal with date
 library(lubridate)
 library(xts)
+library(dplyr)
+
 install.packages("lubridate")
 install.packages("xts")
 #Read data into uber data frame
@@ -21,7 +23,7 @@ install.packages("xts")
 uber_Data<-read.csv("Uber Request Data.csv")
 head(uber_Data)
 # COnvert the date time to proper format using parse_Date_Time function available in lubridate
-
+#--------------------Data Cleaning and deriving new variables----------------------------------------------------------
 uber_Data$Request.timestamp <- parse_date_time(x = uber_Data$Request.timestamp, orders = c("d/m/Y H:M","d-m-Y H:M:S"), locale = "eng")
 uber_Data$Drop.timestamp <- parse_date_time(x= uber_Data$Drop.timestamp,orders = c("d/m/Y H:M","d-m-Y H:M:S"), locale = "eng")
 # Creatin seperate columns for request time and response time
@@ -29,3 +31,23 @@ uber_Data$Drop.timestamp <- parse_date_time(x= uber_Data$Drop.timestamp,orders =
 uber_Data$Request.Time<- format(as.POSIXct(uber_Data$Request.timestamp) ,format = "%H:%M:%S") 
 uber_Data$Drop.Time<- format(as.POSIXct(uber_Data$Drop.timestamp),format = "%H:%M:%S")
 
+#Finding travel time
+uber_Data$Travel_Time<-  uber_Data$Drop.timestamp -uber_Data$Request.timestamp 
+
+#Finding numbler of incomplete rides 
+number_Of_Incomplete_rides<- sum(is.na(uber_Data$Travel_Time))
+
+#Finding mean travel time. Initially filter only completed status data
+
+completed_Rides<- filter(uber_Data, uber_Data$Status=="Trip Completed")
+mean_Travel_Time<- mean(completed_Rides$Travel_Time)
+#mean travel time for completed status is 52.41375 mins
+
+plot(completed_Rides$Travel_Time)
+
+#--------------------------------Analysis-------------------------------------------------
+
+# write the new uber data into csv for visualizing using tableau
+write.csv(uber_Data, file = "Uber_Data_AfterCleaning.csv")
+
+#----- time slots creating is Done in Tableau---------------------------------------------
